eeb386b80a45cbf397d238cb25a925ba7085fba0
Emmanuel Goudot <Emman.uel.Goudot@gmail.com>
2024-02-07 15:05:09 +0100

    Create test_all.py
    re...

---
+++ test_all.py
@@ -0,0 +1,17 @@
+import demo
+
+def test_one():
+	assert demo.fibonacci(0) == 0
+	assert demo.fibonacci(1) == 1
+	assert demo.fibonacci(2) == 1
+	assert demo.fibonacci(10) == 55
+	
+def test_two():
+	assert demo.test1() == None
+	
+def test_prime():
+	assert demo.est_premier(2) == False
+	assert demo.est_premier(5) == True
+	assert demo.est_premier(7) == True
+	assert demo.est_premier(8) == False
+	assert demo.est_premier(100000007) == True
